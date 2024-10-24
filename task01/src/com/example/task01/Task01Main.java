160cef48db61515a38f913104e7d9350d4d332f5
Alexey Postnov <adpostnov@yandex.ru>
2024-10-24 21:42:04 +0500

    Постнов Алексей. Задание 01

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -11,11 +11,14 @@ public class Task01Main {
         /*
         System.out.println(checkSumOfStream(new ByteArrayInputStream(new byte[]{0x33, 0x45, 0x01})));
         */
-
     }
 
     public static int checkSumOfStream(InputStream inputStream) throws IOException {
-        // your implementation here
-        return 0;
+        if (inputStream == null) throw new IllegalArgumentException();
+        int cSum = 0, value = 0;
+        while ((value = inputStream.read()) > -1) {
+            cSum = Integer.rotateLeft(cSum, 1) ^ value;
+        }
+        return cSum;
     }
 }
